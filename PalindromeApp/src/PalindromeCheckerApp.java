4294b0ca8d26e16de4d648518fb65f1583542235
dp605-boop <dp5001@srmist.edu.in>
2026-02-24 16:14:04 +0530

    [dp605-boop] Added UC9

--- PalindromeApp/src/PalindromeCheckerApp.java
+++ PalindromeApp/src/PalindromeCheckerApp.java
@@ -1,23 +1,21 @@
-import java.util.Stack;
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "madam";
-        Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
-        }
-
-        String reversed = "";
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
-        }
-
-        if (input.equals(reversed)) {
+        if (check(input, 0, input.length() - 1)) {
             System.out.println(input + " is a palindrome.");
         } else {
             System.out.println(input + " is not a palindrome.");
         }
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+        return check(s, start + 1, end - 1);
     }
 }
